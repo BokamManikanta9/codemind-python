951ddd8a7da746444db11bf8ba6c0161adde1504
BokamManikanta9 <119719201+BokamManikanta9@users.noreply.github.com>
2023-09-03 13:37:24 +0530

    a=int(input()) b=int(input()) if a==1:     a=2     for i in range(a,b):         c=0         for j in range(2,i):             if i%j==0:                 c=1                 break         if c==0:                 print(i) else:     for i in range(a,b):         c=0         for j in range(2,i):             if i%j==0:                 c=1                 break         if c==0:                 print(i)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,13 +1,21 @@
-def prime(n):
-    
-    if(n<=1):
-         return False   
-    for i in range(2,int(n**0.5)+1):
-        if(n%i==0):
-            return False
-    return True
 a=int(input())
 b=int(input())
-for n in range(a,b):
-    if(prime(n)):
-        print(n)
+if a==1:
+    a=2
+    for i in range(a,b):
+        c=0
+        for j in range(2,i):
+            if i%j==0:
+                c=1
+                break
+        if c==0:
+                print(i)
+else:
+    for i in range(a,b):
+        c=0
+        for j in range(2,i):
+            if i%j==0:
+                c=1
+                break
+        if c==0:
+                print(i)
